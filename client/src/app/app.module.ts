3df276ab76783c1b5a6f7359cc1f587aee38bcce
hefyang <hefei.yang-1@student.uts.edu.au>
2018-10-13 19:16:40 +1100

    Generate component footer Finish the footer html page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
