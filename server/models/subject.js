8db241093eea746715d13a93897cbd66634ab8fc
hefyang <hefei.yang-1@student.uts.edu.au>
2018-10-13 19:59:12 +1100

    Add the subject table

---
+++ server/models/subject.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+const sequelize = require('./db');
+//const Section = require('./section');
+
+// Define the schema of subjects table
+const Subjects = sequelize.define('subject', {
+    id: {
+        type: Sequelize.NUMERIC,
+        primaryKey: true,
+        unique: true,
+        allowNull: false
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    credit: {
+        type: Sequelize.NUMERIC,
+        allowNull: true
+    },
+    type: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    info: {
+        type: Sequelize.STRING,
+        allowNull: true
+    }
+}, {
+    timestamps: false
+});
+
+//Subjects.hasMany(Section);
+
+module.exports = Subjects;
