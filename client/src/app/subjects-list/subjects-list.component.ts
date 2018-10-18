5678cd609ed241f37f0432c4960439e96e518d8e
hefyang <hefei.yang-1@student.uts.edu.au>
2018-10-19 08:57:59 +1100

    Update subjects-list component with functionality

--- client/src/app/subjects-list/subjects-list.component.ts
+++ client/src/app/subjects-list/subjects-list.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subject} from "../_models/subject";
+import {NavigationEnd, Router} from "@angular/router";
+import {TimetableService} from "../_services/timetable.service";
+import {AuthService} from "../_services/auth.service";
 
 @Component({
   selector: 'app-subjects-list',
   templateUrl: './subjects-list.component.html',
   styleUrls: ['./subjects-list.component.css']
 })
-export class SubjectsListComponent implements OnInit {
+export class SubjectsListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subjects: Subject[];
+  searchText: string = '';
+  isLoggedIn: boolean;
+  studentId: number;
+  subjectCount: number;
 
-  ngOnInit() {
+  // ... your class variables here
+  navigationSubscription;
+
+  constructor(
+    private router: Router,
+    private timetableService: TimetableService,
+    public authService: AuthService) {
+
+    // subscribe to the router events - storing the subscription so
+    // we can unsubscribe later.
+    this.navigationSubscription = this.router.events.subscribe((e: any) => {
+      // If it is a NavigationEnd event re-initalise the component
+      if (e instanceof NavigationEnd) {
+        this.initialiseInvites();
+      }
+    });
+  }
+
+  initialiseInvites() {
+    // Set default values and re-fetch any data you need.
+    this.studentId = Number(localStorage.getItem("student_id")) || null;
+
+    this.timetableService.getSubjectCount(this.studentId)
+      .subscribe(count => {
+        this.subjectCount = count.count;
+      });
+
+    this.timetableService.getSubjects(this.studentId)
+      .subscribe((subjects) => {
+        this.subjects = subjects;
+      });
+
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
+
+  enroll(event, id) {
+    event.stopPropagation();
+    event.preventDefault();
+    this.router
+      .navigate(['/',{outlets: {primary: 'timetable', sidebar: ['selector', id]}}])
+      .then(() => { });
+  }
+
+  ngOnDestroy(): void {
+    // avoid memory leaks here by cleaning up after ourselves. If we
+    // don't then we will continue to run our initialiseInvites()
+    // method on every navigationEnd event.
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
+    }
+  }
+
+  ngOnInit(): void {
   }
 
 }
