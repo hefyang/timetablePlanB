39d29aa465bf46be4cd154ea0a2a9951b5584823
William Yang <William.yang2008@yahoo.com>
2018-10-19 02:07:19 +1100

    Update login component with login and authentication functionalities

---
+++ client/src/app/_services/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+import * as moment from "moment";
+import 'rxjs/add/operator/do';
+import "rxjs-compat/add/operator/shareReplay";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+
+  login(studentId:number, password:string) {
+    return this.http.post<{[key:string]: any}>('/api/login', {studentId, password})
+      .do(res => AuthService.setSession(res))
+      .shareReplay();
+  }
+
+  private static setSession(authResult) {
+    const expiresAt = moment().add(authResult.expiresIn, 'minutes');
+
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem("student_id", authResult.studentId);
+    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()))
+  }
+
+  public logout() {
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("student_id");
+    localStorage.removeItem("expires_at");
+  }
+
+  public isLoggedIn() {
+    return moment().isBefore(AuthService.getExpiration());
+  }
+
+  public isLoggedOut() {
+    const expiration = localStorage.getItem("expires_at");
+    const expiresAt = JSON.parse(expiration);
+    return !moment(expiresAt).isValid();
+  }
+
+  private static getExpiration() {
+    const expiration = localStorage.getItem("expires_at");
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
+  }
+}
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../_services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,7 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  confirmed: {[key:string]: string};
+  message: string;
+  clicked: boolean;
+
+  constructor(
+    private fb:FormBuilder,
+    private authService:AuthService,
+    private router:Router) {
+
+    this.clicked = false;
+
+    this.form = this.fb.group({
+      studentId: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    const val = this.form.value;
+
+    this.clicked = true;
+
+    if (val.studentId && val.password) {
+      this.authService.login(val.studentId, val.password)
+        .subscribe(
+          (res) => {
+            if (res.idToken) {
+              this.router.navigate([''])
+                .then(() => {
+                  console.log("User is logged in");
+                });
+            } else {
+              if (res.unauthorized) this.message = "The user name or password you entered is incorrect.";
+              if (res.unconfirmed) this.message = "Please confirm your email";
+              // console.log("Please confirm your email");
+            }
+          }
+        )
+    }
+  }
 
   ngOnInit() {
   }
