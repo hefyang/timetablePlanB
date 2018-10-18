daa0a66ee91c7088ffc68208513c97acd8083730
William Yang <William.yang2008@yahoo.com>
2018-10-19 09:39:21 +1100

    Update nav bar component with functionality

--- client/src/app/nav-bar/nav-bar.component.ts
+++ client/src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentChecked, Component} from '@angular/core';
+import {AuthService} from "../_services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements AfterContentChecked {
 
-  constructor() { }
+  isLoggedIn: boolean;
 
-  ngOnInit() {
+  constructor(public authService: AuthService, private router: Router) { }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  ngAfterContentChecked(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    if (!this.isLoggedIn) this.authService.logout();
   }
 
 }
