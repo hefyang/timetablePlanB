ec6883be32cb7caae9a56eaed94980f93c139fe0
hefyang <hefei.yang-1@student.uts.edu.au>
2018-10-19 09:21:54 +1100

    Update description component with functionality

--- client/src/app/description/description.component.ts
+++ client/src/app/description/description.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {TimetableService} from "../_services/timetable.service";
 
 @Component({
   selector: 'app-description',
   templateUrl: './description.component.html',
   styleUrls: ['./description.component.css']
 })
-export class DescriptionComponent implements OnInit {
+export class DescriptionComponent implements OnInit, OnDestroy {
+  subjectId: number;
+  subjectTitle: string;
+  subjectInfo: string;
 
-  constructor() { }
+  // ... your class variables here
+  navigationSubscription;
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private timetableService: TimetableService,
+    private router: Router) {
+
+    // subscribe to the router events - storing the subscription so
+    // we can unsubscribe later.
+    this.navigationSubscription = this.router.events.subscribe((e: any) => {
+      // If it is a NavigationEnd event re-initalise the component
+      if (e instanceof NavigationEnd) {
+        this.initialiseInvites();
+      }
+    });
+
+  }
+
+  initialiseInvites() {
+
+    this.subjectId = this.route.snapshot.params['id'];
+    this.timetableService.getSubject(this.subjectId)
+      .subscribe(subject => {
+        this.subjectTitle = subject.title;
+        this.subjectInfo = subject.info;
+        document.getElementById("description")
+          .innerHTML = this.subjectInfo;
+      });
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+    // avoid memory leaks here by cleaning up after ourselves. If we
+    // don't then we will continue to run our initialiseInvites()
+    // method on every navigationEnd event.
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
+    }
   }
 
 }
