f7f7d2b9458d8e527babb7dc3db0f3163b7705e1
William Yang <William.yang2008@yahoo.com>
2018-10-19 01:52:27 +1100

    Add registration service to the client end

---
+++ client/src/app/_services/registration.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Student} from "../_models/student";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+
+  constructor(private http: HttpClient) { }
+
+  public validateStudentId(studentId: string): Observable<any> {
+    return this.http.post<{[key:string]: boolean}>(`api/user/`, {studentId: studentId});
+  }
+
+  public register(student: Student) {
+    return this.http.post<{[key:string]: boolean}>('api/register', student)
+  }
+}
