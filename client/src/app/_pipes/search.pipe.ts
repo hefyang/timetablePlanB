812cb9279c09147e9c5faa0675b99ec8011d88c6
hefyang <hefei.yang-1@student.uts.edu.au>
2018-10-19 09:18:28 +1100

    Add search filter to the client end

---
+++ client/src/app/_pipes/search.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Subject} from "../_models/subject";
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+  transform(items: Subject[], searchText: string): any {
+    if(!items) return [];
+    if(!searchText) return items;
+    searchText = searchText.toLowerCase();
+    return items.filter( it => {
+      return it.title.toLowerCase().includes(searchText) || it.id.toString().includes(searchText);
+    });
+  }
+
+}
