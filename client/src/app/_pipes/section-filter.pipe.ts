7d48abeed47183e63f1e736203da38ca215b7c9d
hefyang <hefei.yang-1@student.uts.edu.au>
2018-10-19 09:29:54 +1100

    Add section filter pipe to client end

---
+++ client/src/app/_pipes/section-filter.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Section} from "../_models/section";
+
+@Pipe({
+  name: 'filter'
+})
+export class SectionFilterPipe implements PipeTransform {
+
+  transform(items: Section[], types: string): any {
+    if (!items) return [];
+    if (!types) return items;
+    types = types.toLowerCase();
+    return items.filter(it => {
+      return !types.includes(it.type.toLowerCase());
+    });
+  }
+
+}
